c598c13dfa6208200e9b0da7fea94837d8913faf
niksjodhani <nikunjjodhani90@gmail.com>
2022-05-31 23:08:42 +0100

    homework

---
+++ src/test/java/com/restapiexample/dummy/employeeinfo/employeePostTest.java
@@ -0,0 +1,27 @@
+package com.restapiexample.dummy.employeeinfo;
+
+import com.restapiexample.dummy.model.EmployeePojo;
+import com.restapiexample.dummy.testbase.TestBase;
+import io.restassured.response.Response;
+import org.junit.Test;
+
+import static io.restassured.RestAssured.given;
+
+public class employeePostTest extends TestBase {
+
+    @Test
+    public void createEmployeeData(){
+        EmployeePojo employeePojo = new EmployeePojo();
+        employeePojo.setName("NameTest");
+        employeePojo.setSalary("1111");
+        employeePojo.setAge("44");
+        employeePojo.setId("25");
+
+        Response response = given()
+                .body(employeePojo)
+                .when()
+                .post("/create");
+        response.then().statusCode(200);
+        response.prettyPrint();
+    }
+}
